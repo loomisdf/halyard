b25b82a053797c3be065930b25608d89e5bcc503
Daltin Loomis <daltin.loomis@target.com>
2017-08-16 11:57:30 -0500

    create ldap command

---
+++ halyard-cli/src/main/java/com/netflix/spinnaker/halyard/cli/command/v1/config/security/authn/ldap/LdapCommand.java
@@ -0,0 +1,11 @@
+package com.netflix.spinnaker.halyard.cli.command.v1.config.security.authn.ldap;
+
+import com.netflix.spinnaker.halyard.cli.command.v1.config.security.authn.AuthnMethodCommand;
+import com.netflix.spinnaker.halyard.config.model.v1.security.AuthnMethod;
+
+public class LdapCommand extends AuthnMethodCommand {
+  public AuthnMethod.Method getMethod() {
+    return AuthnMethod.Method.LDAP;
+  }
+
+}
